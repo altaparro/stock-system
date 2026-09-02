02aa3e8fe44eadf2b35eeac26123b0e097876d3a
matias <m.altaparro@spb.gba.gov.ar>
2026-09-02 16:36:53 -0300

    Corregir grilla del formulario de producto (grid de 4 columnas)

--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -346,7 +346,7 @@ function ContenidoProductos() {
             </div>
           )}
 
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-6">
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
             <div>
               <label className={claseLabel}>Marca</label>
               <input
